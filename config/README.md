6de48ea76aebdf52b56fcd78c21ab40384cb012e
Yun-Yan Chi <jaiyalas@gmail.com>
2013-02-26 12:11:53 +0800

    merge repo:dot-rc to here by hand

---
+++ config/README.md
@@ -0,0 +1,18 @@
+# dot-rc
+
+* My system config on 
+  * Mac OS X 10.7.5
+
+## Newest updated Date
+
+  * 2013/02/26
+
+### Additional Information
+
+  * used vimrc, as my vim setting, from
+    * [https://github.com/rueshyna/vimrc]
+
+## History
+
+  * System: Mac OS X 10.6.4
+  * Updated on: 2010/08/02
---
+++ config/_config/fish/config.fish
@@ -0,0 +1,55 @@
+########################
+# Jaiyalas @ MacBook
+########################
+# Mac OS X 10.6
+# 2010/07/28
+########################
+
+##### Environment #####
+
+set LC_CTYPE "en_US.UTF-8"
+
+set TERM xterm-256color
+
+set -u CLASSPATH
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+set -x PATH ~/bin
+set -x PATH /bin $PATH
+set -x PATH /sbin $PATH
+set -x PATH /usr/bin $PATH
+set -x PATH /usr/lib $PATH
+set -x PATH /usr/include $PATH
+set -x PATH /usr/sbin $PATH
+set -x PATH /usr/X11R6/bin $PATH
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/lib $PATH
+set -x PATH /usr/local/include $PATH
+set -x PATH ~/.cabal/bin $PATH
+
+function fish_prompt -d 'custom prompt'
+  set -x TERM xterm-256color
+  prompt.rb
+end
+
+set -x CDPATH .
+set -x PROMPT_SHELL fish
+
+##### Function #####
+
+function rm
+  rm-alt $argv
+end
+
+function farm
+  cd ~/project/iis/code_farm
+end
+
+function aq
+  open /Applications/Aquamacs.app/ $argv
+end
+
+function confish
+  vim ~/.config/fish/config.fish
+end
