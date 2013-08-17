e4852a240d70ea4202ac57db758b2cc59a965d15
Yun-Yan Chi <jaiyalas@gmail.com>
2013-08-17 21:15:34 +0800

    fixed catch error; ignoring .git/ ;added ANSI color

--- tlist.hs
+++ tlist.hs
@@ -3,7 +3,9 @@ module Main (main) where
   import Data.List
   import System.Directory
   import System.Environment
-  
+
+  import System.Console.ANSI 
+ 
   main = do
     args <- getArgs
     mapM_ tlist (if null args then ["."] else args)
@@ -11,18 +13,25 @@ module Main (main) where
   tlist path =
     visit (if "/" `isPrefixOf` path then "" else ".") "" "" "" path
   
+  visit _ _ _ _ ".git" = do
+    setSGR [SetColor Foreground Vivid Yellow]
+    putStrLn "A .git/ directory have been detected "
+    setSGR [Reset]
+
   visit path leader tie arm node = do
     putStrLn (leader ++ arm ++ tie ++ node)
     visitChildren (path ++ "/" ++ node) (leader ++ extension)
-      where extension = case arm of "" -> ""; "`" -> " "; _ -> "| "
+      where extension = case arm of ""  -> ""; "`" -> "    "; _   -> "|   "
   
   visitChildren path leader =
     whenM (doesDirectoryExist path) $ do
       contents <- getDirectoryContents path
-        `catch` (\e -> return [show e])
+        -- `catch` (\e -> return [show e])
       let visibles = sort . filter (`notElem` [".", ".."]) $ contents
           arms = replicate (length visibles - 1) "|" ++ ["`"]
       zipWithM_ (visit path leader "-- ") arms visibles
   
   whenM mtest ma = mtest >>= flip when ma
   
+
+
